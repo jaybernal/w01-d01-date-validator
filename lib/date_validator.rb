01b7ee684f69775bee943fdcba3d15f833a49e36
Jay Bernal <jjbernal@gmail.com>
2016-03-14 22:01:42 -0500

    reworked the assignment

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -15,32 +15,33 @@
 # This method should, in its final form, not do any output.
 
 def valid_date?(month, day, year)
-  #write your method here
-  months_30 = [4, 6, 9, 11]
-  months_31 = [1, 3, 5, 7, 8, 10, 12]
-
-  if month < 1 || month > 12
-		return false
-	end
-
-	if (months_31.include? month) && !(day < 32 && day > 0)
-		return false
-	elsif (months_30.include? month) && !(day < 31 && day > 0)
-		return false
-	end
-	if (year % 4 == 0 && month == 2) && (year % 100 != 0) && !(day > 0 && day < 30)
-			return false
-	elsif (year % 4 == 0 && month == 2) && (year % 100 == 0 && year % 400 == 0) && !(day > 0 && day < 30)
-	        return false
-    elsif (month == 2) && (day > 0 && day < 29)
-			return true
-	end
-
-	if year < 1880 || year > 2280
-		return false
-    end
     
-    return true    
-end
+# determine the days in month
+    month_31 = [1,3,5,7,8,10,12]
+    month_30 = [4,6,9,11]
+    
+# determines all the leap years    
+     leap_year = (1880..2280).find_all {|i| i % 400 == 0 || i % 4 == 0 }  
+    
+# if years are within the range
+     if year < 1880 || year > 2280
+         false   
+     end
+     
+# if days are within the range
+     if day < 0 || day > 31
+         false
+     end
+     
+# finds if months are within the range
+    if month <0 || month > 12
+        false
+    end
+
+
+
+end  
+
+valid_date?(2,29,1880)
 
 
