7f1af2d5a03227fdda1c44c66a73fa6ec0bff90b
Jay Bernal <jjbernal@gmail.com>
2016-03-19 21:49:10 -0500

    in the middle of the refactoring process, oops

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -1,68 +1,66 @@
-# This method takes three arguments: month, date, and year, and returns either TRUE
-# or FALSE (the boolean values, not the string representation of those boolean
-# values) depending on whether the date is valid (i.e. does exist on the calendar)
-# or is not valid (i.e. does not exist on the calendar).
-#
-# Here are the rules:
-#
-# + The month must be between 1 and 12 (inclusive)
-# + The year must be between 1880 and 2280 (inclusive)
-# + The day must exist within the month
-# + You must account for leap years (look up how leap years work)
-# + No using the `Date` class (we'll refactor to allow the `Date` class later this week)
-# + Only worry about integer inputs (no floats, strings, etc.)
-#
-# This method should, in its final form, not do any output.
-=begin
 def valid_date?(month, day, year)
-    
-   
-
-# find leap year
-    if (month == 2 && leap_year.include?(year)) && day == 29
-      return true
-    elsif month == 2 && day > 28
-     	return false
-    end
-
-  return true
-end
-=end 
-
-def assign_variables(month_31,month_30,leap_year)
+  valid_date = true
+# determine the days in month
   month_31 = [1,3,5,7,8,10,12]
   month_30 = [4,6,9,11]
-  leap_year = (1880..2280).find_all { |i| i % 4 == 0 && i % 100 != 0 || i % 400 == 0 }
-end
 
-def validate_year(year)
-  if year < 1880 || year > 2280
-  	return false   
-  end
-end
+  # determines all the leap years    
+  leap_year = leap_year_array(year)
+ 
+  # if years are within the range
+  valid_year = validate_year(year)
+    if valid_year == false
+      valid_date = false
+    end
 
-def validate_month(month)
-	if month < 0 || month > 12
-  	return false
-  end
-end
+  # finds if months are within the range
+  valid_month = validate_month(month)
+    if valid_month == false
+      valid_date = false
+    end
 
-def validate_days_in_month(month)
- 	if month_31.include?(month) && day > 31 || day <= 0
-   return false
+  # find days and months validation
+  if month_31.include?(month) && day > 31 || day <= 0
+    valid_date = false
   end
 
   if month_30.include?(month) && day > 30 || day <= 0
-   return false
+    valid_date = false
   end
+
+  # find leap year
+  valid_leap_year = validate_leap_year(month, leap_year, year, day)
+    if valid_leap_year == false
+      valid_date = false
+    end
+
+  valid_date
 end
 
-def is_leap_year(month, year)
+
+
+
+
+def validate_year(year)
+  year >= 1880 && year <= 2280
+end
+
+def validate_month(month)
+  month >= 1 && month <= 12
+end
+
+def leap_year_array(year)
+ year = (1880..2280).find_all do |i| 
+  i % 4 == 0 && 
+  i % 100 != 0 || 
+  i % 400 == 0
+ end 
+end
+
+def validate_leap_year(month, leap_year, year, day)
   if (month == 2 && leap_year.include?(year)) && day == 29
-   return true
+    valid_date = true
   elsif month == 2 && day > 28
-   return false
+    valid_date = false
   end
 end
-
-
